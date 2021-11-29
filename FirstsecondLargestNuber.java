538b3a243a085a0afb3c00830b59b5dcbfce3aa7
Achyuth Naidu <ashwath.bly@gamil.com>
2021-11-29 22:16:04 +0530

    Day-3 pratices Problem rev->0.0.1

--- FirstsecondLargestNuber.java
+++ FirstsecondLargestNuber.java
@@ -1,24 +1,37 @@
-import java.util.Arrays;  
-public class FirstsecondLargestNuber{  
+import java.util.*;  
+public class FirstsecondLargestNuber{    
+    public static void main(String args[]){  
+    int n;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the elements size: ");
+    n = sc.nextInt();
+    int arr[] = new int[n];//10
+    
+    System.out.println("Enter the array elements: ");
+    for (int i = 0; i <n; i++) 
+        arr[i]= sc.nextInt();// 1 2 3 4 5 6 7 8 9 10
 
-public static int getSecondLargest(int[] a, int total){
+
     int temp;
-    for (int i = 0; i < total; i++)   
-        {  
-            for (int j = i + 1; j < total; j++)   
-            {  
-                if (a[i] > a[j])   
-                {  
-                    temp = a[i];  
-                    a[i] = a[j];  
-                    a[j] = temp;  
-                }  
-            }  
-        }  
-       return a[total-2];   
-}  
-public static void main(String args[]){  
+    for (int i = 0; i < n; i++) {//0
+        for (int j = i+1; j < n; j++) {//1
+            if (arr[i]>arr[j]) {//(1>2)
 
-int a[]={1,2,5,6,3,2};  
-System.out.println("Second Largest: "+getSecondLargest(a,6));  
-}}  
+                temp= arr[i];//1
+                arr[i]=arr[j];//2
+                arr[j]=temp;//1
+            }    
+        }
+        
+    }
+
+    System.out.println("Array elements after sorting");
+    for (int i = 0; i < n; i++) {
+        System.out.println(" "+arr[i]);
+    }
+System.out.print("Seconf largest number in give array is: "+arr[n-2] );//
+
+    // int a[]={1,2,5,6,3,2};  
+// System.out.println("Second Largest: "+getSecondLargest(a,6));  
+    }
+}  
